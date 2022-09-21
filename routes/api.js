85ee95edffe3cd7ba00fb33f5bc7527e65e54cb4
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-21 14:37:57 -0300

    updates

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,11 @@ const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 router.post("/fileanalyse", upload.single('upfile'), function (req, res) {
+    const resObj = {
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    }
     res.json(req.file);
 });
 
