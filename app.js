492c6ca3d75209fcd9e801d459df2bfe65966a8d
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-21 14:43:20 -0300

    Updates

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
+
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
